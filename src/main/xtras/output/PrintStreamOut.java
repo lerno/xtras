0041819372156f678fc7bad04160138a18012de9
christoffer.lerno <christoffer.lerno@cd4e00a8-21f3-11de-9d52-39b120432c5d>
2009-04-12 12:10:34 +0000

    Added first draft of output classes

---
+++ src/main/xtras/output/PrintStreamOut.java
@@ -0,0 +1,26 @@
+package xtras.output;
+
+import java.io.PrintStream;
+
+/** @author Christoffer Lerno */
+class PrintStreamOut implements Out
+{
+	private final PrintStream m_stream;
+
+	public PrintStreamOut(PrintStream stream)
+	{
+		m_stream = stream;
+	}
+
+	public void print(String s)
+	{
+		m_stream.print(s);
+		m_stream.flush();
+	}
+
+	public void println()
+	{
+		m_stream.println();
+		m_stream.flush();
+	}
+}
---
+++ src/main/xtras/output/PrintWriterOut.java
@@ -0,0 +1,25 @@
+package xtras.output;
+
+import java.io.PrintWriter;
+
+/** @author Christoffer Lerno */
+class PrintWriterOut implements Out
+{
+	private final PrintWriter m_writer;
+	public PrintWriterOut(PrintWriter writer)
+	{
+		m_writer = writer;
+	}
+
+	public void print(String s)
+	{
+		m_writer.write(s);
+		m_writer.flush();
+	}
+
+	public void println()
+	{
+		m_writer.println();
+		m_writer.flush();
+	}
+}
